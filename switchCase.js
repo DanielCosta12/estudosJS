9f8c8a52588047ceef13a6b44b7b34dc5d74b4fd
DanielCosta12 <danieldossantosdacosta@gmail.com>
2022-09-01 23:13:42 -0300

    adicionando switchCase

---
+++ switchCase.js
@@ -0,0 +1,13 @@
+function getAnimal(id){
+    switch(id){
+        case 1: return "cão";
+        case 2: return "gato";
+        case 3: return "cobra";
+        default: return "Adicione o id certo de 1 até 3";
+    }
+}
+
+console.log(getAnimal(3));
+console.log(getAnimal(1));
+console.log(getAnimal(2));
+console.log(getAnimal(84));
